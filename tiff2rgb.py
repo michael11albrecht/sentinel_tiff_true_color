73abb9a85db171aeb58153544b5fc3175613feeb
Michael Albrecht <albrecht@fortiss.org>
2024-04-11 12:19:15 +0200

    del old methods + added arguments

--- tiff2rgb.py
+++ tiff2rgb.py
@@ -1,9 +1,9 @@
 import rasterio
 import numpy as np
-from PIL import Image, ImageEnhance
 import os
 from tqdm import tqdm
 from rgb_js import rgbAdjustment
+import argparse
 
 class Tiff2Rgb:
     def __init__(self, input_file, output_dir):
@@ -16,50 +16,6 @@ class Tiff2Rgb:
         bands_np = np.array(band)
         return bands_np[3], bands_np[2], bands_np[1] # RGB
     
-    def brighten(self, band):
-        alpha=0.07
-        beta=5
-        return np.clip(alpha*band+beta, 0,255)
-    
-    def normalize(self, band):
-        band_min, band_max = (band.min(), band.max())
-        return ((band-band_min)/((band_max - band_min)))
-    
-    def gammacorr(self, band):
-        gamma=2.5
-        return np.power(band, 1/gamma)
-    
-    def br_manual(self, bands):
-        e_bands = []
-        for band in bands:
-            band = self.brighten(band)
-            band = self.normalize(band)
-            e_bands.append(band)
-        rgb = np.dstack((e_bands[0], e_bands[1], e_bands[2]))
-        return Image.fromarray((rgb * 255).astype(np.uint8))
-    
-    def gamma(self, bands):
-        e_bands = []
-        for band in bands:
-            band = self.gammacorr(band)
-            band = self.normalize(band)
-            e_bands.append(band)
-        rgb = np.dstack((e_bands[0], e_bands[1], e_bands[2]))
-        return Image.fromarray((rgb * 255).astype(np.uint8))
-    
-    def br_pil(self, bands):
-        e_bands = []
-        for band in bands:
-            band = self.normalize(band)
-            e_bands.append(band)
-        rgb = np.dstack((e_bands[0], e_bands[1], e_bands[2]))
-        img = Image.fromarray((rgb * 255).astype(np.uint8))
-        brighter = ImageEnhance.Brightness(img)
-        brighter = brighter.enhance(5)
-        brighter = ImageEnhance.Color(brighter)
-        brighter = brighter.enhance(0.5)
-        return brighter
-    
     def save(self, img, prefix=''):
         os.makedirs(self.output_dir, exist_ok=True)
         filename = self.input_file.split('/')[-1].split('.')[0:-1]
@@ -67,17 +23,15 @@ class Tiff2Rgb:
         img.save(f'{self.output_dir}/{prefix}_{filename}'+'.png')
 
 if __name__ == '__main__':
-    input_dir = 'data/tiff'
-    output_dir = 'data/png'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str, default='data/tiff')
+    parser.add_argument('--output_dir', type=str, default='data/png')
+    args = parser.parse_args()
+    input_dir = args.input_dir
+    output_dir = args.output_dir
     for tif in tqdm(os.listdir(input_dir), desc='Processing'):
         if tif.endswith('.tiff') or tif.endswith('.tif'):
             tiff2rgb = Tiff2Rgb(f'{input_dir}/{tif}', output_dir)
             bands = tiff2rgb.extract()
-            '''img = tiff2rgb.br_manual(bands)
-            tiff2rgb.save(img,'m')
-            img = tiff2rgb.gamma(bands)
-            tiff2rgb.save(img,'g')
-            img = tiff2rgb.br_pil(bands)
-            tiff2rgb.save(img,'p')'''
             img = rgbAdjustment().evaluate_pixel(bands[0]/10000, bands[1]/10000, bands[2]/10000)
-            tiff2rgb.save(img,'r')
+            tiff2rgb.save(img,'n')
